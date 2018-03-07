f63b4509417dfcc9e62e1c75993aa31549224d6c
LOGAN CHAPMAN <1632377@student.fife.ac.uk>
2018-03-07 10:10:23 +0000

    Added New Sprites and A Logging Player Script

---
+++ Example Unity Game/Example Unity Game/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public string toPrintClass = "Well what country is this, the United Arab Emirates? There's lots of sweaty cameltoe and everyone's a terrorist.";
+
+	// Use this for initialization
+	void Start () {
+		string toPrint = "We are here to drink your beer, and steal your rum at the point of a gun. Your alcohol to us will fall, cos' we are here to drink your beer. Drink!";
+			Debug.Log (toPrintClass);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
