5eecd18ddaebdcc46e314bd896dfb5df7f42fc4a
LOGAN CHAPMAN <1632377@student.fife.ac.uk>
2018-03-07 10:48:30 +0000

    Added things to script

--- Example Unity Game/Example Unity Game/Assets/Scripts/Player.cs
+++ Example Unity Game/Example Unity Game/Assets/Scripts/Player.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 	public string toPrintClass = "Well what country is this, the United Arab Emirates? There's lots of sweaty cameltoe and everyone's a terrorist.";
+	public float speed = 5;
 
 	// Use this for initialization
 	void Start () {
 		string toPrint = "We are here to drink your beer, and steal your rum at the point of a gun. Your alcohol to us will fall, cos' we are here to drink your beer. Drink!";
-			Debug.Log (toPrintClass);
+		Debug.Log (toPrintClass);
+		string returned1 = TestFunction ("This is just a test message, please ingore this.", 42);
+		string returned2 = TestFunction ("This is just a test message, please ingore this.", 73);
+		Debug.Log ("returned1 = " + returned1);
+		Debug.Log ("returned2 = " + returned2);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Rigidbody2D ourRigidBody = GetComponent<Rigidbody2D> ();
+		ourRigidBody.velocity = Vector2.right * speed;
+	}
+
+	string TestFunction(string message, int count) {
+		Debug.Log (message + " " + count);
+
+		return "Return of the String";
 	}
 }
