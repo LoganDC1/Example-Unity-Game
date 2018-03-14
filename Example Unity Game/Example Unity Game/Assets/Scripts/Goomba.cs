380819b7f300e46ad77fdbd1517686005d183cc9
LOGAN CHAPMAN <1632377@student.fife.ac.uk>
2018-03-14 10:28:24 +0000

    Added a new script, Code to deal damage to Mario and code to make Mario invulnerable when he takes damage

---
+++ Example Unity Game/Example Unity Game/Assets/Scripts/Goomba.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goomba : MonoBehaviour {
+
+	// Public variables (visible in editor)
+	public float damage = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Called when trigger collides with another collider
+	void OnTriggerStay2D(Collider2D other){
+
+		// Attempt to get the Player script from the thing we collided with
+		Player playerScript = other.GetComponent<Player> ();
+
+		// If the player script exists (aka it was actually on the thing we collided with)
+		if (playerScript != null) {
+			// Call the Damage function on the Player script and pass our damage variable as the amount
+			playerScript.Damage (damage);
+
+			Debug.Log ("Goomba dealt damage to player = " + damage);
+		}
+	}
+}
--- Example Unity Game/Example Unity Game/Assets/Scripts/Player.cs
+++ Example Unity Game/Example Unity Game/Assets/Scripts/Player.cs
@@ -4,17 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	public string toPrintClass = "Well what country is this, the United Arab Emirates? There's lots of sweaty cameltoe and everyone's a terrorist.";
 	public float speed = 5;
+	public float health = 100;
+	public float invulnerableDuration = 1;
+
+	private float invulnerableEndTime = 0;
 
 	// Use this for initialization
 	void Start () {
-		string toPrint = "We are here to drink your beer, and steal your rum at the point of a gun. Your alcohol to us will fall, cos' we are here to drink your beer. Drink!";
-		Debug.Log (toPrintClass);
-		string returned1 = TestFunction ("This is just a test message, please ingore this.", 42);
-		string returned2 = TestFunction ("This is just a test message, please ingore this.", 73);
-		Debug.Log ("returned1 = " + returned1);
-		Debug.Log ("returned2 = " + returned2);
 	}
 	
 	// Update is called once per frame
@@ -23,9 +20,22 @@ public class Player : MonoBehaviour {
 		ourRigidBody.velocity = Vector2.right * speed;
 	}
 
-	string TestFunction(string message, int count) {
-		Debug.Log (message + " " + count);
+	public void Damage(float damageToDeal) {
 
-		return "Return of the String";
+		if (Time.time >= invulnerableEndTime) {
+			// Reducing health by the damage passed in
+
+			health = health - damageToDeal;
+
+			// TODO: handle death
+
+			// Set us as invulnerable for a set duration
+			invulnerableEndTime = Time.time + invulnerableDuration;
+
+			// Log the result of the function
+			Debug.Log("Damage was dealt");
+			Debug.Log ("damageToDeal = " + damageToDeal);
+			Debug.Log("health = " +health);
+		}
 	}
 }
